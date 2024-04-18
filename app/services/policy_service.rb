cbd5ae273788934d33ba8548d591ed6d817e28e2
Guilherme Silva <guilherme.gss@outlook.com.br>
2024-04-18 16:53:00 -0300

    adiciona endpoint de listagem

--- app/services/policy_service.rb
+++ app/services/policy_service.rb
@@ -4,6 +4,24 @@ class PolicyService
   end
 
   def all
+    response = JSON.parse(graphql_call(query_all).body)
+    policies = response["data"]["policies"]
+  end
+
+  def self.all
+    new.all
+  end
+
+  private
+
+  def graphql_call(query, variables = {})
+    @conn.post "/graphql" do |req|
+      req.headers[:content_type] = "application/json"
+      req.body = { query: query, variables: variables }.to_json
+    end
+  end
+
+  def query_all
     query =
       <<-Query
         query {
@@ -13,18 +31,13 @@ class PolicyService
               name
               cpf
             }
+          vehicle {
+            licensePlate
           }
+          effectiveDate
+          expirationDate
         }
+      }
       Query
-
-    response = @conn.post "/graphql" do |req|
-      req.headers[:content_type] = "application/json"
-      req.body = { query: query, variables: {} }.to_json
-    end
   end
-
-  def self.all
-    new.all
-  end
-
 end
--- spec/requests/index_policies_spec.rb
+++ spec/requests/index_policies_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Index Policies', type: :request do
   describe 'GET /policies' do
-    let(:query) do 
+    let(:query) do
       <<-Query
         query {
           policies {
@@ -11,22 +11,62 @@ describe 'Index Policies', type: :request do
               name
               cpf
             }
+          vehicle {
+            licensePlate
           }
+          effectiveDate
+          expirationDate
         }
+      }
       Query
     end
 
+    let(:graphql_response) do
+      {
+        "data" => {
+          "policies" => [
+            {
+              "policyId" => "1",
+              "insuredPerson" => {
+                "name" => "Phil Foden",
+                "cpf" => "123.456.789-10"
+              },
+              "vehicle" => {
+                "licensePlate" => "ABC1D23"
+              },
+              "effectiveDate" => "2024-01-01",
+              "expirationDate" => "2025-01-01"
+            },
+            {
+              "policyId" => "2",
+              "insuredPerson" => {
+                "name" => "Edson Arantes",
+                "cpf" => "123.654.987-10"
+              },
+              "vehicle" => {
+                "licensePlate" => "BDF2E34"
+              },
+              "effectiveDate" => "2024-01-01",
+              "expirationDate" => "2025-01-01"
+            }
+          ]
+        }
+      }
+
+    end
 
     before do
       stub_request(:post, "insurance-graphql:4000/graphql")
         .with(body: {query: query, variables: {} }.to_json)
+        .to_return(body: graphql_response.to_json, status: 200)
     end
 
     it 'views policies with success' do
       get policies_path
 
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include("Número da Apólice: 01")
+      expect(response.body).to include("Phil Foden")
+      expect(response.body).to include("Edson Arantes")
     end
   end
 end
