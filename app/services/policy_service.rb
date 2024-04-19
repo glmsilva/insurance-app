c6801adf84d11250c14dd9a1bdd483861b945a63
Guilherme Silva <guilherme.gss@outlook.com.br>
2024-04-19 12:47:11 -0300

    refatora policy service

--- app/services/policy_service.rb
+++ app/services/policy_service.rb
@@ -1,43 +1,25 @@
 class PolicyService
-  def initialize
+  def initialize(query:, variables: {})
     @conn = Faraday.new(url: "http://insurance-graphql:4000")
+    @query = query
+    @variables = variables
   end
 
   def all
-    response = JSON.parse(graphql_call(query_all).body)
+    response = JSON.parse(graphql_call.body)
     policies = response["data"]["policies"]
   end
 
   def self.all
-    new.all
+    new(query: QueryBuilderService.policies).all
   end
 
   private
 
-  def graphql_call(query, variables = {})
+  def graphql_call
     @conn.post "/graphql" do |req|
       req.headers[:content_type] = "application/json"
-      req.body = { query: query, variables: variables }.to_json
+      req.body = { query: @query, variables: @variables }.to_json
     end
-  end
-
-  def query_all
-    query =
-      <<-Query
-        query {
-          policies {
-            policyId
-            insuredPerson {
-              name
-              cpf
-            }
-          vehicle {
-            licensePlate
-          }
-          effectiveDate
-          expirationDate
-        }
-      }
-      Query
   end
 end
---
+++ app/services/query_builder_service.rb
@@ -0,0 +1,44 @@
+class QueryBuilderService
+  def self.policies
+    <<-Query
+        query {
+          policies {
+            policyId
+            insuredPerson {
+              name
+              cpf
+            }
+          vehicle {
+            licensePlate
+          }
+          effectiveDate
+          expirationDate
+        }
+      }
+    Query
+  end
+
+  def self.policy
+    <<-Query
+      query {
+        Policy($policyId: Int!) {
+          policy(PolicyId: $policyId) {
+            policyId
+            insuredPerson {
+              name
+              cpf
+            }
+            vehicle {
+              brand
+              model
+              year
+              licensePlate
+            }
+            effectiveDate
+            expirationDate
+          }
+        }
+      }
+    Query
+  end
+end
